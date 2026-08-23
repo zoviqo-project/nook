1316dcd639c3ddb78d40f5c5c5aecb80c3031941
albertesteveferres-ctrl <albertesteveferres@gmail.com>
2026-08-23 20:25:12 +0200

    Remove location selector from cafe choices

--- ios/Nook/Features/BookingMap.swift
+++ ios/Nook/Features/BookingMap.swift
@@ -194,10 +194,6 @@ struct CoffeeShopsView: View {
           action: { withAnimation(NookMotion.spring) { showMap.toggle() } }
         ) {
           VStack(spacing: 8) {
-          if app.selectedCoffeeMatch == nil {
-            placeModeSelector
-              .padding(.horizontal, 12)
-          }
           Group {
             if showMap { mapView } else { listView }
           }
